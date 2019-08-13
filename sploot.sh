fabd8e416b20ea04d57421a9876fea4d70e2b988
Suresh Kumar Devaraj <sureshkumar.d@oasys.co>
2019-08-13 08:02:54 +0000

    Update sploot.sh

--- sploot.sh
+++ sploot.sh
@@ -1,18 +1,20 @@
-
 forever stopall
 
 cd apiGateway/
+sudo npm install
 npm start
 
 echo "Apigateway Server Started...."
 cd ../
 
 cd mobileApp/
+sudo npm install
 npm start
 echo "Mobile App Server Started...."
 cd ../
 
 cd adminApp/
+sudo npm install
 npm start
 echo "Admin App Server Started...."
 cd ../
