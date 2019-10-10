f04447e272ccebf3ed46bfaf64a858cbfbb3c14d
jayaprakash s <jayaprakash.s@troondx.com>
2019-10-10 19:40:30 +0530

    Sh file changes....

--- sploot.sh
+++ sploot.sh
@@ -4,19 +4,19 @@ pm2 delete all
 
 cd apiGateway/
 npm install -d
-pm2 start index.js
+pm2 start gatewayindex.js
 echo "Apigateway Server Started...."
 cd ../
 
 cd mobileApp/
 npm install -d
-pm2 start index.js
+pm2 start mobileindex.js
 echo "Mobile App Server Started...."
 cd ../
 
 cd adminApp/
 npm install -d
-pm2 start index.js
+pm2 start adminindex.js
 echo "Admin App Server Started...."
 cd ../
 
